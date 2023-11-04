76abf028ba86258c603174680f320cb38528113e
Chan-Heee <cksgml019@naver.com>
2023-11-04 23:16:34 +0900

    feat/스케쥴링 활성화 설정

--- src/main/java/com/StudyOps/domain/penalty/service/StudyPenaltyService.java
+++ src/main/java/com/StudyOps/domain/penalty/service/StudyPenaltyService.java
@@ -10,6 +10,7 @@ import com.StudyOps.domain.penalty.entity.StudyPenalty;
 import com.StudyOps.domain.penalty.repository.StudyPenaltyRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
---
+++ src/main/java/com/StudyOps/global/common/MySchedulingConfig.java
@@ -0,0 +1,10 @@
+package com.StudyOps.global.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+//스케쥴링 활성화 설정
+@Configuration
+@EnableScheduling
+public class MySchedulingConfig {
+}
