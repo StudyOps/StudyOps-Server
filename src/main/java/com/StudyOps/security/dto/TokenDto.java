d3be0f5f6c7bb97f07db4c7c5faff5a43d6edeff
Chan-Heee <cksgml019@naver.com>
2023-12-29 00:36:32 +0900

    feat/ RefreshToken 쿠키로 구현 및 Token 재발급 로직 수정

--- src/main/java/com/StudyOps/security/dto/TokenDto.java
+++ src/main/java/com/StudyOps/security/dto/TokenDto.java
@@ -1,15 +1,16 @@
 package com.StudyOps.security.dto;
 
-import lombok.*;
+import com.StudyOps.security.entity.RefreshToken;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class TokenDto {
-
-    private String grantType;
-    private String accessToken;
-    private String refreshToken;
-    private Long accessTokenExpiresIn;
+    private TokenResDto tokenResDto;
+    private RefreshToken refreshToken;
 }
--- src/main/java/com/StudyOps/security/jwt/TokenProvider.java
+++ src/main/java/com/StudyOps/security/jwt/TokenProvider.java
@@ -1,6 +1,9 @@
 package com.StudyOps.security.jwt;
 
 import com.StudyOps.security.dto.TokenDto;
+import com.StudyOps.security.dto.TokenResDto;
+import com.StudyOps.security.entity.RefreshToken;
+import com.StudyOps.security.repository.RefreshTokenRepository;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -14,6 +17,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.security.Key;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -21,6 +26,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @Component
 public class TokenProvider {
+    private final RefreshTokenRepository refreshTokenRepository;
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "Bearer";
@@ -29,9 +35,11 @@ public class TokenProvider {
 
     private final Key key;
 
-    public TokenProvider(@Value("${jwt.secret}") String secretKey) {
+    public TokenProvider(@Value("${jwt.secret}") String secretKey,
+                         RefreshTokenRepository refreshTokenRepository) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
     public TokenDto generateTokenDto(Authentication authentication) {
@@ -52,19 +60,49 @@ public class TokenProvider {
                 .compact();
 
         // Refresh Token 생성
-        String refreshToken = Jwts.builder()
+        String refreshTokenValue = Jwts.builder()
                 .setExpiration(new Date(now + REFRESH_TOKEN_EXPIRE_TIME))
                 .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
 
+        RefreshToken refreshToken = RefreshToken.builder()
+                .key(authentication.getName())
+                .value(refreshTokenValue)
+                .build();
+
         return TokenDto.builder()
-                .grantType(BEARER_TYPE)
-                .accessToken(accessToken)
-                .accessTokenExpiresIn(accessTokenExpiresIn.getTime())
+                .tokenResDto(TokenResDto.builder()
+                        .grantType(BEARER_TYPE)
+                        .accessToken(accessToken)
+                        .accessTokenExpiresIn(accessTokenExpiresIn.getTime())
+                        .build())
                 .refreshToken(refreshToken)
                 .build();
     }
 
+    public TokenResDto generateNewAccessToken(Authentication authentication) {
+
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = (new Date()).getTime();
+
+        // Access Token 생성
+        Date accessTokenExpiresIn = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+        String accessToken = Jwts.builder()
+                .setSubject(authentication.getName())       // payload "sub": "name"
+                .claim(AUTHORITIES_KEY, authorities)        // payload "auth": "ROLE_USER"
+                .setExpiration(accessTokenExpiresIn)        // payload "exp": 1516239022 (예시)
+                .signWith(key, SignatureAlgorithm.HS512)    // header "alg": "HS512"
+                .compact();
+
+        return TokenResDto.builder()
+                .grantType(BEARER_TYPE)
+                .accessToken(accessToken)
+                .accessTokenExpiresIn(accessTokenExpiresIn.getTime())
+                .build();
+    }
     public Authentication getAuthentication(String accessToken) {
         // 토큰 복호화
         Claims claims = parseClaims(accessToken);
