c30ce116de5427756551093ac4647b03cf10df94
Chan-Heee <cksgml019@naver.com>
2023-12-30 22:19:17 +0900

    feat/로그인 시 잘못된 기입 에러 처리

--- src/main/java/com/StudyOps/global/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/StudyOps/global/common/exception/CustomExceptionHandler.java
@@ -1,21 +1,35 @@
 package com.StudyOps.global.common.exception;
 
 import com.StudyOps.global.common.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.ObjectOutput;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(CustomRuntimeException.class)
-    public ResponseEntity<ApiResponse<Object>> handleException(CustomRuntimeException e){
+    public ResponseEntity<ApiResponse<Object>> handleCustomRuntimeException(CustomRuntimeException e){
         ApiResponse<Object> response = ApiResponse.builder()
                 .isSuccess(false)
                 .status(e.getStatus().value())
                 .message(e.getMessage())
                 .build();
         return ResponseEntity.status(e.getStatus()).body(response);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiResponse<Object>> handleBadCredentialsException(){
+        ApiResponse<Object> response = ApiResponse.builder()
+                .isSuccess(false)
+                .status(401)
+                .message("아이디 혹은 패스워드가 틀렸습니다.")
+                .build();
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 
 }
