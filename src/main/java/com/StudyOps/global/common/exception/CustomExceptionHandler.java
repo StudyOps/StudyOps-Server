d13c64102a1fe38cc32ba1a34385c3ccd5684b94
Chan-Heee <cksgml019@naver.com>
2024-01-01 21:28:12 +0900

    refac/ 아이디 또는 비밀번호 오류 에러코드400으로 수정

--- src/main/java/com/StudyOps/global/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/StudyOps/global/common/exception/CustomExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.StudyOps.global.common.exception;
 
 import com.StudyOps.global.common.ApiResponse;
-import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
